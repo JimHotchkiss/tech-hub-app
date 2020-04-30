17d3acd2e25c925edf1c39eeee2f59aa66f815cc
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-04-30 10:23:59 -0700

    Adding functionality to the home page. When a user selects a feature, the feature page is rendered, and the navbar is updated to show the icon and description of the selected feature and a home icon.

--- javascript/money-settings.js
+++ javascript/money-settings.js
@@ -5,48 +5,54 @@ document.addEventListener("DOMContentLoaded", function () {
 const iconData = [
   {
     data: "msa",
-    src: "/img/msa-tile-white.png",
-    name: "Money Settings"
+    src: "/img/msa2.png",
+    name: "Money Settings",
   },
   {
     data: "networking",
-    src: "/img/networking.png",
-    name: "Networking"
+    src: "/img/networking2.png",
+    name: "Networking",
   },
   {
     data: "color-adjustments",
-    src: "/img/wrench.png",
-    name: "Color Adjustment"
+    src: "/img/adjustment.png",
+    name: "Color Adjustment",
   },
   {
     data: "ipad",
     src: "/img/ipad.png",
-    name: "iPad Assistance"
+    name: "iPad Assistance",
   },
   {
     data: "tech-spec",
     src: "/img/ifu.png",
-    name: "Tech Specs"
+    name: "Tech Specs",
   },
   {
     data: "reprocessing",
     src: "/img/reprocessing.png",
-    name: "Reprocessing"
-  }
+    name: "Reprocessing",
+  },
 ];
 
 const iconButtons = () => {
-  iconData.map(item => {
+  iconData.map((item, index) => {
+    console.log(index);
     const topCardDiv = document.getElementById("card-div");
     const cardClassDiv = document.createElement("div");
     cardClassDiv.setAttribute("class", "card-wrapper");
+    cardClassDiv.setAttribute("dataset-console", item.data);
+    cardClassDiv.addEventListener("click", (event) => {
+      showFeature(event);
+    });
     const aTagImgDiv = document.createElement("div");
-    aTagImgDiv.setAttribute("class", "a-tag-img-div");
+    aTagImgDiv.setAttribute("class", "icon-div");
     const imgElement = document.createElement("img");
     imgElement.setAttribute("src", item.src);
+    imgElement.setAttribute("class", "img-element");
     imgElement.setAttribute("alt", "icon");
     const cardAnchor = document.createElement("a");
-    cardAnchor.setAttribute("data-console", item.data);
+    // cardAnchor.setAttribute("data-console", item.data);
     cardAnchor.appendChild(imgElement);
     cardClassDiv.appendChild(cardAnchor);
     topCardDiv.appendChild(cardClassDiv);
@@ -57,5 +63,23 @@ const iconButtons = () => {
     textContainer.appendChild(pTag);
     cardClassDiv.appendChild(textContainer);
     topCardDiv.appendChild(cardClassDiv);
+  });
+};
+
+const showFeature = (event) => {
+  event.path.map((item) => {
+    if (item.src !== undefined) {
+      const titleDiv = document.getElementById("nav-title");
+      console.log(item);
+      titleDiv.id = "title-no-show";
+      // const titleDiv = document.getElementsByClassName("nav-title");
+      // titleDiv.className = "nav-title-feature";
+      // const featureIconDiv = document.createElement("div");
+      // featureIconDiv.setAttribute("class", "feature-icon-nav");
+      // const iconImgElement = document.createElement("img");
+      // iconImgElement.setAttribute("src", item.src);
+      // featureIconDiv.appendChild(iconImgElement);
+      // navBar.prepend(featureIconDiv);
+    }
   });
 };
